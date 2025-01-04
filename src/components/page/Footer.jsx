064ceb18c082f57570eaaa91f56738ec485b97d7
Fabiano_Dantas <fabianorsdantas@gmail.com>
2025-01-04 16:40:24 -0300

    Alterando rodapé

--- src/components/page/Footer.jsx
+++ src/components/page/Footer.jsx
@@ -1,28 +1,32 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-
-import CircularBar from "./CircularBar";
+import { Link, useLocation } from "react-router-dom";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
 
 export default function Footer() {
+  const location = useLocation();
+
   return (
     <StyledFooter>
-      <NavLink to="/habits">Hábitos</NavLink>
-      <NavLink to="/today">
-        <CircularBar />
-      </NavLink>
-      <NavLink to="/history">Histórico</NavLink>
+      <Button to="/habits" isActive={location.pathname === "/habits"}>
+        <CalendarMonthIcon />
+        <span>Hábitos</span>
+      </Button>
+      <TodayButton to="/today" isActive={location.pathname === "/today"}>
+        <EventAvailableIcon />
+        <span>Hoje</span>
+      </TodayButton>
     </StyledFooter>
   );
 }
 
 const StyledFooter = styled.footer`
   width: 100vw;
-  height: 70px;
+  height: 65px;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 5vw;
 
   position: fixed;
   left: 0;
@@ -31,9 +35,44 @@ const StyledFooter = styled.footer`
   background: #fff;
 `;
 
-const NavLink = styled(Link)`
-  font-size: 18px;
-  line-height: 22px;
+const Button = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-  color: #52b6ff;
+  flex: 1; /* Faz os botões ocuparem todo o espaço disponível */
+  height: 100%; /* Ocupa toda a altura do rodapé */
+
+  background-color: ${(props) => (props.isActive ? "#52b6ff" : "#fff")};
+  color: ${(props) => (props.isActive ? "#fff" : "#52b6ff")};
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: bold;
+
+  border: none; /* Remove bordas desnecessárias */
+  cursor: pointer;
+  transition: background-color 0.3s ease, color 0.3s ease;
+
+  &:hover {
+    background-color: ${(props) => (props.isActive ? "#4aa0e0" : "#f0f8ff")};
+    color: ${(props) => (props.isActive ? "#fff" : "#52b6ff")};
+  }
+
+  &:not(:last-child) {
+    border-right: 1px solid #52b6ff; /* Adiciona uma borda entre os botões */
+  }
+
+  & > svg {
+    margin-right: 8px; /* Espaço entre o ícone e o texto */
+  }
+`;
+
+const TodayButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  & > svg {
+    margin-bottom: 5px; /* Espaço entre o ícone e o texto "Hoje" */
+  }
 `;
